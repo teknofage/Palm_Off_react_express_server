f4ebab891187b53e56754cb5e5ee8377692588ba
teknofage <teknofage@gmail.com>
2020-12-10 19:10:08 -0800

    changed message, removed button

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 // Test this route with:
 app.get('/about', (req, res) => {
   // This Object is converted to JSON and returned.
-  res.json({ about: 'This service generates a random number.' })
+  res.json({ about: 'This service pulls data about hundreds of thousands of food and gives up-to-date information about the allergen and dietary status of ingredients, and whether it contains any palm oil product.' })
 })
 
 // Random number route
