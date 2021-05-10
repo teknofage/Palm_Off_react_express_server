335ea020c0e33f4580e0940c7a4bafd7c2fd8b44
teknofage <teknofage@gmail.com>
2021-05-10 01:02:45 -0700

    updated user model

--- models/user.js
+++ models/user.js
@@ -14,4 +14,26 @@ const UserSchema = new Schema({
   {timestamps: {createdAt: 'created_at'}}
 );
 
+// Must use function here! ES6 => functions do not bind this!
+UserSchema.pre("save", function (next) {
+    // ENCRYPT PASSWORD
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// Need to use function to enable this.password to work.
+UserSchema.methods.comparePassword = function (password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch);
+    });
+};
+
 module.exports = mongoose.model("User", UserSchema);
