51962c519b89267c65e2b8b765b3a5560e6c10d7
teknofage <teknofage@gmail.com>
2021-05-10 01:08:48 -0700

    added reddit-db file and installed jsonwebtoken

---
+++ data/reddit-db.js
@@ -0,0 +1,23 @@
+/* Mongoose Connection */
+const mongoose = require("mongoose");
+assert = require("assert");
+require("dotenv").config()
+
+const url = "mongodb://localhost/reddit-db";
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false,
+  },
+  function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to database");
+
+    // db.close(); turn on for testing
+  }
+);
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
